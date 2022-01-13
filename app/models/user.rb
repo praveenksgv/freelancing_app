a5d184bb3b4f6b0babe50205d13fed9f57decc9e
praveenksgv <praveenksgv@gmail.com>
2022-01-13 10:49:01 +0530

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
 #ApplicationRecord class inherites ActiveRecord::Base
 #Which means that User class will have all the functionality of ActiveRecord::Base 
     before_save {self.email = email.downcase }
@@ -11,11 +12,42 @@ class User < ApplicationRecord
     has_secure_password 
     validates :password, presence: true, length: {minimum: 6}
 
+
+    
+    
     # Returns the hash digest of the given string.
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-  ​  end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+
+#Return a random tocken
+    def User.new_token 
+       SecureRandom.urlsafe_base64
+    end
+
+#Remember a user in the database for in persistent sessions.
+    def remember 
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+      remember_digest
+    end
+#Retuens a session token to prevent session hijacking
+#We resuse the remember digest for convenience.
+    def session_token
+      remember_digest || remember
+    end
+
+#Returns true if the given token matches the digest
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
 
 end
