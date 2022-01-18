c234ec078a9bf9fb752f058934ba3689f041b5c5
praveenksgv <praveenksgv@gmail.com>
2022-01-18 09:43:31 +0530

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,17 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users do
+    member do 
+      get :following, :followers
+    end
+  end
 
   resources :users
   resources :account_activation, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts,      only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   
 
 end
